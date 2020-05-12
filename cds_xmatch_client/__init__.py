9d8ae67004ff339715e60a1803368c0d2d858bc9
Ernesto <ernesto.cast.nav@gmail.com>
2020-05-12 15:29:29 -0400

    Exception handling added

--- cds_xmatch_client/__init__.py
+++ cds_xmatch_client/__init__.py
@@ -1,4 +1,5 @@
 import io
+import sys
 import pandas as pd
 import requests
 
@@ -12,29 +13,32 @@ class XmatchClient:
 
 	def execute(self,df: pd.core.frame.DataFrame, extcatalog: str, distmaxarcsec: int = 1) -> (pd.core.frame.DataFrame):
 
-		extcatalog = CATALOG_MAP[extcatalog]
-		# Encode input
-		subset = df[ ['ra','dec','oid']]
-		subset.rename(columns={ 'ra' : 'ra_in', 'dec' : 'dec_in' }, inplace=True)
-		s = io.StringIO()
-		subset.to_csv(s)
-		table_str = s.getvalue()
+		try:
+			extcatalog = CATALOG_MAP[extcatalog]
+			# Encode input
+			subset = df[ ['ra','dec','oid']]
+			subset.rename(columns={ 'ra' : 'ra_in', 'dec' : 'dec_in' }, inplace=True)
+			s = io.StringIO()
+			subset.to_csv(s)
+			table_str = s.getvalue()
 
-		# Send the request
-		r = requests.post('http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
-		data={
-		    'request': 'xmatch',
-		    'distMaxArcsec': distmaxarcsec,
-		    'selection': 'best',
-		    'RESPONSEFORMAT': 'csv',
-		    'cat2': extcatalog,
-		    'colRA1': 'ra_in',
-		    'colDec1': 'dec_in'},
-		files={'cat1': table_str})
+			# Send the request
+			r = requests.post('http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
+			data={
+			    'request': 'xmatch',
+			    'distMaxArcsec': distmaxarcsec,
+			    'selection': 'best',
+			    'RESPONSEFORMAT': 'csv',
+			    'cat2': extcatalog,
+			    'colRA1': 'ra_in',
+			    'colDec1': 'dec_in'},
+			files={'cat1': table_str})
 
-		# Decode output
-		print(r.text)
-		bytes_response = io.BytesIO(r.content)
-		result = pd.read_csv(bytes_response)
+			# Decode output
+			bytes_response = io.BytesIO(r.content)
+			result = pd.read_csv(bytes_response)
+		except Exception as exception:
+			sys.stderr.write( "Request to CDS xmatch failed: %s \n"%(exception) )
+			raise exception
 
 		return result
