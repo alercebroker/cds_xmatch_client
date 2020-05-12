4545c1c754b01f2c98f20a1bcc27c8dea7f6d5ac
Ernesto <ernesto.cast.nav@gmail.com>
2020-05-12 12:50:52 -0400

    vizier catalog's shorcuts added

--- cds_xmatch_client/__init__.py
+++ cds_xmatch_client/__init__.py
@@ -3,41 +3,38 @@ import pandas as pd
 import requests
 
 CATALOG_MAP = {
-		'allwise' : 'ALLWISE',
-		'gaia-dr2' : 'GAIA DR2',
-		'sdss-dr12' : '',
+		'allwise' : 'vizier:II/328/allwise',
+		'gaia-dr2' : 'vizier:I/345/gaia2',
+		'sdss-dr12' : 'vizier:V/147/sdss12'
 }
 
 class XmatchClient:
 
-    def __init__(self,url):
-        self.url = url
+	def execute(self,df: pd.core.frame.DataFrame, extcatalog: str, distmaxarcsec: int = 1) -> (pd.core.frame.DataFrame):
 
-    def execute(self,df: pd.core.frame.DataFrame,
-        	        extcatalog: str,
-		            distmaxarcsec: int = 1) -> (list, list):
+		extcatalog = CATALOG_MAP[extcatalog]
+		# Encode input
+		subset = df[ ['ra','dec','oid']]
+		subset.rename(columns={ 'ra' : 'ra_in', 'dec' : 'dec_in' }, inplace=True)
+		s = io.StringIO()
+		subset.to_csv(s)
+		table_str = s.getvalue()
 
-	extcatalog = CATALOG_MAP[extcatalog]
-        subset = df[ ['ra','dec','oid']]
-        subset.rename(columns={ 'ra' : 'ra_in', 'dec' : 'dec_in' }, inplace=True)
-        s = io.StringIO()
-        subset.to_csv(s)
-
-        table_str = s.getvalue()
-        # Send the request!
-        r = requests.post('http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
+		# Send the request
+		r = requests.post('http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
 		data={
 		    'request': 'xmatch',
 		    'distMaxArcsec': distmaxarcsec,
 		    'selection': 'best',
 		    'RESPONSEFORMAT': 'csv',
-		    'typeCat2' : 'Vizier',
 		    'cat2': extcatalog,
 		    'colRA1': 'ra_in',
 		    'colDec1': 'dec_in'},
 		files={'cat1': table_str})
 
-        bytes_response = io.BytesIO(r.content)
-        result = pd.read_csv(bytes_response)
+		# Decode output
+		print(r.text)
+		bytes_response = io.BytesIO(r.content)
+		result = pd.read_csv(bytes_response)
 
-        return result
+		return result
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cds_xmatch_client",
+    version="0.1",
+    packages=find_packages(),
+    author="Ernesto Castillo N.",
+    scripts=["scripts/cdsxmatch"]
+)
