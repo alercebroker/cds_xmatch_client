b5dbd6b2fddbfd535563a1c6094589ae108ea0a4
Ernesto <ernesto.cast.nav@gmail.com>
2020-05-18 20:52:16 -0400

    votable and columns added

--- cds_xmatch_client/__init__.py
+++ cds_xmatch_client/__init__.py
@@ -1,6 +1,8 @@
 import io
 import sys
 import pandas as pd
+from astropy.table import Table
+import astropy.io.votable as votable
 import requests
 import json
 import pkg_resources
@@ -12,38 +14,95 @@ with open(input_path) as catalogs_file:
 
 class XmatchClient:
 
-	def execute(self,df: pd.core.frame.DataFrame, extcatalog: str, selection: str, distmaxarcsec: int = 1) -> (pd.core.frame.DataFrame):
+	def execute(
+			self,
+			catalog, 
+			catalog_type: str, 
+			ext_catalog: str,
+			ext_columns: list,
+			selection: str,
+			result_type: str,
+			distmaxarcsec: int = 1
+	):
 
 		try:
 			#catalog alias
-			if extcatalog in CATALOG_MAP:
-				extcatalog = CATALOG_MAP[extcatalog]
+			if ext_catalog in CATALOG_MAP:
+				ext_catalog = CATALOG_MAP[ext_catalog]
 
 			# Encode input
-			new_columns = {}
-			for c in df.columns:
-				new_columns[c]="%s_in"%(c)
-			df.rename(columns=new_columns, inplace=True)
-			s = io.StringIO()
-			df.to_csv(s)
-			table_str = s.getvalue()
+			catalog_content = None
+			if catalog_type == 'pandas':
+
+				string_io = io.StringIO()
+				new_columns = {}
+				for c in catalog.columns:
+					new_columns[c]="%s_in"%(c)
+				catalog.rename(columns=new_columns, inplace=True)
+
+				catalog.to_csv(string_io)
+				catalog_content = string_io.getvalue()
+
+			elif catalog_type == 'astropy':
+
+				columns = list(catalog.columns)
+				for c in columns:
+					catalog.rename_column(c,"%s_in"%(c))
+
+				bytes_io = io.BytesIO()
+				catalog = votable.from_table(catalog)
+				votable.writeto(catalog,bytes_io)
+				catalog_content = bytes_io.getvalue()
+
+			else: 
+				raise Exception("Unknow input type %s"%(catalog_type) )
+
+			#columns
+			ext_columns_str = None
+			if not ext_columns is None:
+				ext_columns_str = ",".join(ext_columns)
+
+			#response format
+			response_format = None
+			if result_type == 'pandas':
+				response_format = 'csv'
+			elif result_type == 'astropy':
+				response_format = 'votable'
+			else:
+				raise Exception("Unknow output type %s"%(result_type) )
+
+			#params
+			params = {
+					   'request': 'xmatch',
+					   'distMaxArcsec': distmaxarcsec,
+					   'selection': selection,
+					   'RESPONSEFORMAT': response_format,
+					   'cat2': ext_catalog,
+					   'colRA1': 'ra_in',
+					   'colDec1': 'dec_in'
+			}
+			if ext_columns_str is not None:
+				params['cols2'] = ext_columns_str
 
 			# Send the request
-			r = requests.post('http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
-			data={
-			    'request': 'xmatch',
-			    'distMaxArcsec': distmaxarcsec,
-			    'selection': selection,
-			    'RESPONSEFORMAT': 'csv',
-			    'cat2': extcatalog,
-			    'colRA1': 'ra_in',
-			    'colDec1': 'dec_in'},
-			files={'cat1': table_str})
+			r = requests.post(
+								'http://cdsxmatch.u-strasbg.fr/xmatch/api/v1/sync',
+								data=params,
+								files= {'cat1' : catalog_content}
+			)
 
 			# Decode output
-			bytes_response = io.BytesIO(r.content)
-			result = pd.read_csv(bytes_response)
+			response_bytes = io.BytesIO(r.content)
+			result = None
+			if result_type == 'pandas':
+				result = pd.read_csv(response_bytes)
+
+			elif result_type == 'astropy':
+				result = Table.read(response_bytes)
+
+
 		except Exception as exception:
+
 			sys.stderr.write( "Request to CDS xmatch failed: %s \n"%(exception) )
 			raise exception
 
