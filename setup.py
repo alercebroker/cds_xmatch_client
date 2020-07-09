66b6b60aa7c02f5f2e5952859f12893856ea64d5
Camilo Valenzuela <cacke91@gmail.com>
2020-07-09 16:49:03 -0400

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     packages=find_packages(),
     author="Ernesto Castillo N.",
     scripts=["scripts/cdsxmatch"],
+    install_requires=[
+     "requests >= 2.20",
+     "pandas >=1.0",
+     "astropy >= 3.2"
+    ],
     package_data = { 'cds_xmatch_client' : [ 'data/catalog_alias.json' ] }
 )
